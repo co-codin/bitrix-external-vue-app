cfbe9c5da0fffcbf14c0c3275fd7b1405c2aaaa5
Lenar Khusainov <work@lenarx.ru>
2022-02-18 16:48:36 +0300

    Add background to alert

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <page-header h1="Настройки" :breadcrumbs="breadcrumbs" />
-    <v-alert dense type="info" outlined>
-      В этом разделе будут настройки приложений
-    </v-alert>
+    <v-card>
+      <v-alert dense type="info" outlined>
+        В этом разделе будут настройки приложений
+      </v-alert>
+    </v-card>
   </div>
 </template>
 
