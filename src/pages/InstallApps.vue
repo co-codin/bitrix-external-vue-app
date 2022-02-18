78c5fbf5b39e4697e834c24b07f5139966d5fb7d
Lenar Khusainov <work@lenarx.ru>
2022-02-19 00:42:14 +0300

    Install app page fix

--- src/pages/InstallApps.vue
+++ src/pages/InstallApps.vue
@@ -45,16 +45,20 @@ export default {
   data: () => ({
     apps: [
       {
-        name: 'Файлы к задачам',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores consequuntur cum dicta, dolorem eligendi expedita placeat porro quod temporibus? Dignissimos et hic id iusto libero modi sed sunt temporibus unde vitae! Ad corporis ducimus molestias, perspiciatis quam quia ut. A, adipisci alias animi autem beatae consectetur consequuntur, ducimus eveniet excepturi exercitationem fuga in iure magnam maxime nisi optio pariatur porro quaerat quisquam quod repellat saepe soluta suscipit voluptas voluptatum? Accusamus atque dignissimos id natus odit praesentium vel velit. Aspernatur beatae explicabo molestias, odio quae quis velit? Accusantium, amet culpa, cumque dicta doloremque neque nulla porro repellendus sapiente, unde velit.',
-        placement: 'task.tabs',
+        name: 'Вкладка "Документы" в задачах',
+        buttonLabel: 'Документы',
+        description: 'Загрузка документов к задачам',
+        placement: 'TASK_VIEW_TAB',
+        handler: '/tasks/documents',
         installed: false
       },
       {
-        name: 'Создание подзадач закупщика',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores consequuntur cum dicta, dolorem eligendi expedita placeat porro quod temporibus? Dignissimos et hic id iusto libero modi sed sunt temporibus unde vitae! Ad corporis ducimus molestias, perspiciatis quam quia ut. A, adipisci alias animi autem beatae consectetur consequuntur, ducimus eveniet excepturi exercitationem fuga in iure magnam maxime nisi optio pariatur porro quaerat quisquam quod repellat saepe soluta suscipit voluptas voluptatum? Accusamus atque dignissimos id natus odit praesentium vel velit. Aspernatur beatae explicabo molestias, odio quae quis velit? Accusantium, amet culpa, cumque dicta doloremque neque nulla porro repellendus sapiente, unde velit.',
-        placement: 'task.tabs',
-        installed: true
+        name: 'Создание подзадач закупщиком',
+        buttonLabel: 'Постпродажный процесс',
+        description: 'Создание подзадач закупщиком к базовой задаче',
+        placement: 'TASK_LIST_CONTEXT_MENU',
+        handler: '/tasks/create-supplier-tasks',
+        installed: false
       }
     ],
     breadcrumbs: [{ text: 'Установка приложений' }]
@@ -71,7 +75,12 @@ export default {
   },
   methods: {
     installApp(app) {
-      console.log(app)
+      window.BX24.callMethod('placement.bind', {
+        PLACEMENT: app.placement,
+        HANDLER: app.handler,
+        TITLE: app.buttonLabel,
+        DESCRIPTION: app.description
+      })
     },
     deleteApp(app) {
       console.log(app)
