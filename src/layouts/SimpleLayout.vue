5ba20922fa0305e2906312e219888cdf2cf480ed
Lenar Khusainov <work@lenarx.ru>
2022-02-18 17:16:06 +0300

    Add Bitrix24 library

--- src/layouts/SimpleLayout.vue
+++ src/layouts/SimpleLayout.vue
@@ -7,10 +7,16 @@
 <script>
 export default {
   mounted() {
-    // const recaptchaScript = document.createElement('script')
-    //
-    // recaptchaScript.setAttribute('src', 'https://api.bitrix24.com/api/v1/')
-    // document.head.appendChild(recaptchaScript)
+    const recaptchaScript = document.createElement('script')
+
+    recaptchaScript.setAttribute('src', 'https://api.bitrix24.com/api/v1/')
+    document.head.appendChild(recaptchaScript)
+
+    BX24.init(() => {
+      const initDate = BX24.getAuth()
+
+      console.log('initDate: ', initDate)
+    })
   }
 }
 </script>
