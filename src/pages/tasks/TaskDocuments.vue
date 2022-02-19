7f5a68c87642272146efbee1cae2a9cbac9591e0
Lenar Khusainov <work@lenarx.ru>
2022-02-19 08:02:06 +0300

    Add test uploader

--- src/pages/tasks/TaskDocuments.vue
+++ src/pages/tasks/TaskDocuments.vue
@@ -4,6 +4,8 @@
       <v-alert outlined dense type="info">
         Скоро здесь появится возможность загружать документы
       </v-alert>
+      <input ref="uploader" type="file">
+      <v-btn @click="upload">Загрузить выбранный файл</v-btn>
     </v-card>
   </div>
 </template>
@@ -15,7 +17,18 @@ export default {
     headers: [
 
     ]
-  })
+  }),
+  methods: {
+    uploadFile() {
+      window.BX24.callMethod('disk.storage.uploadfile', {
+        id: 688,
+        fileContent: this.$refs.uploader,
+        data: {
+          NAME: '2102102.jpg'
+        }
+      })
+    }
+  }
 }
 </script>
 
