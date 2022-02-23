1f8d1a19e0062b685cf28f4c7408d3c7b36afae7
ElijahCoding <cuiyeqing960904@gmail.com>
2022-02-23 16:14:02 +0300

    fixed

--- src/pages/tasks/TaskDocuments.vue
+++ src/pages/tasks/TaskDocuments.vue
@@ -202,13 +202,13 @@ export default {
     ]
   }),
   mounted() {
-    window.addEventListener('load', async () => {
-      await this.getTaskFiles()
+    window.addEventListener('load',  () => {
+      this.getTaskFiles()
     })
   },
   methods: {
-    async getTaskFiles() {
-      const { taskId } = await window.BX24.placement.info().options
+    getTaskFiles() {
+      const { taskId } = window.BX24.placement.info().options
 
       window.BX24.callMethod(
         'task.item.getdata',
