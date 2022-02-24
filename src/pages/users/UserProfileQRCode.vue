223618568f17da4dea87b331aad7c8bf016b544b
Lenar Khusainov <work@lenarx.ru>
2022-02-24 14:42:41 +0300

    Select app

--- src/pages/users/UserProfileQRCode.vue
+++ src/pages/users/UserProfileQRCode.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     QR code here... Coming soon
+    <v-text-field
+      @focus="openDialog"
+    />
   </div>
 </template>
 
@@ -12,6 +15,17 @@ export default {
     console.log(BX24)
     console.log(options)
     console.log(this.$route.query)
+  },
+  methods: {
+    openDialog() {
+      BX24.selectCRM({
+        entityType: ['lead', 'contact', 'company', 'deal', 'quote'],
+        multiple: true,
+        value: { lead:[1348,2,35], contact:[2], company:[4,3], deal:[1,2], quote:[1] }
+      }, function(data) {
+        console.log(data)
+      })
+    }
   }
 }
 </script>
