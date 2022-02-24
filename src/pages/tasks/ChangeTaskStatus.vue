2857b5c0ec9082d3538486cdb14e36be0855c9a4
Lenar Khusainov <work@lenarx.ru>
2022-02-24 10:25:29 +0300

    Add template to task status changing

--- src/pages/tasks/ChangeTaskStatus.vue
+++ src/pages/tasks/ChangeTaskStatus.vue
@@ -1,38 +1,69 @@
 <template>
   <div>
-    <page-header h1="Изменение статуса задачи" :show-home-link="false" />
-    <v-card>
-      <v-card-text>
-        <v-form>
-          <v-select
-            label="Статус задачи"
-            :items="taskStatusLabels"
-            placeholder="Выберите статус задачи"
-          />
-        </v-form>
-      </v-card-text>
-    </v-card>
-    <div class="mt-2 text-right">
-      <v-btn color="primary">Изменить статус</v-btn>
-    </div>
+    <page-header h1="Изменение статуса задачи" :show-home-link="false"/>
+    <page-loader v-if="loading"/>
+    <template>
+      <v-form @submit.prevent="changeTaskStatus">
+        <v-card>
+          <v-card-text>
+            <v-form>
+              <v-select
+                label="Статус задачи"
+                :items="taskStatusLabels"
+                placeholder="Выберите статус задачи"
+              />
+            </v-form>
+          </v-card-text>
+        </v-card>
+        <div class="mt-2 text-right">
+          <v-btn color="primary" type="submit">Изменить статус задачи</v-btn>
+        </div>
+      </v-form>
+    </template>
   </div>
 </template>
 
 <script>
 import PageHeader from '../../components/PageHeader'
+import PageLoader from '../../components/PageLoader'
 
 export default {
   components: {
-    PageHeader
+    PageHeader,
+    PageLoader
   },
   data: () => ({
+    loading: true,
     taskStatusLabels: [
       { text: 'В оплате', value: 'В оплате' },
       { text: 'Ждем поставку', value: 'Ждем поставку' },
       { text: 'Отписка', value: 'Отписка' },
       { text: 'Отгрузка', value: 'Отгрузка' },
       { text: 'Установка', value: 'Установка' }
-    ]
-  })
+    ],
+    form: {
+      status: null
+    },
+    task: null
+  }),
+  mounted() {
+    BX24.init(this.loadTask)
+  },
+  methods: {
+    loadTask() {
+      const { taskId } = windowBX24.placement.info().options
+
+      console.log(taskId)
+    },
+    changeTaskStatus() {
+      BX24.callMethod(
+        'tasks.task.update',
+        { taskId: this.task.id, fields: { UF_TASK_STATUS: this.form.status } },
+        () => {
+          BX24.closeApplication()
+        }
+      )
+    }
+  }
 }
 </script>
