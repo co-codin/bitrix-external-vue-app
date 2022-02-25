25baa09b278ff4707fe2ea5a1c4726d04bff8dbf
Lenar Khusainov <work@lenarx.ru>
2022-02-25 21:18:19 +0300

    QR code template

--- src/pages/users/UserProfileQRCode.vue
+++ src/pages/users/UserProfileQRCode.vue
@@ -5,14 +5,36 @@
       Произошла ошибка при загрузке Битрикс 24
     </v-alert>
     <template v-else>
-      <div>
-        <v-btn color="primary">Скачать</v-btn>
-        <v-btn color="primary" class="ml-2">Скопировать</v-btn>
-      </div>
-      <v-card class="mt-2" style="max-width: 200px">
+      <v-card
+        max-width="300"
+      >
         <v-img
-          src="https://ru.qr-code-generator.com/wp-content/themes/qr/new_structure/markets/basic_market/generator/dist/generator/assets/images/websiteQRCode_noFrame.png"
+          class="white--text align-end"
+          height="200px"
+          contain
+          :src="qrCode"
         />
+        <v-divider/>
+        <v-card-actions class="justify-center py-2">
+          <v-btn
+            icon
+            class="pa-3"
+            color="primary"
+            title="Скопировать QR код в буфер обмена"
+            @click="copyImageToClipboard"
+          >
+            <copy-icon width="38" height="38" />
+          </v-btn>
+          <v-btn
+            class="pa-3"
+            icon
+            color="primary"
+            title="Скачать QR код"
+            @click="downloadImage"
+          >
+            <download-icon width="38" height="38" />
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </template>
   </div>
@@ -20,16 +42,25 @@
 
 <script>
 import PageLoader from '@/components/PageLoader'
+import CopyIcon from '@/components/heroicons/CopyIcon'
+import DownloadIcon from '@/components/heroicons/DownloadIcon'
 
 export default {
   components: {
-    PageLoader
+    PageLoader,
+    CopyIcon,
+    DownloadIcon
   },
   data: () => ({
     user: null,
     error: null,
     loading: true
   }),
+  computed: {
+    qrCode() {
+      return '/images/qr.png'
+    }
+  },
 
   mounted() {
     // const options = BX24.placement?.info()
@@ -42,6 +73,13 @@ export default {
         first_name: 'Петр'
       }
       this.loading = false
+    },
+    async copyImageToClipboard() {
+      const response = await fetch(this.qrCode)
+      const blob = await response.blob()
+      const data = [new ClipboardItem({ [blob.type]: blob })]
+
+      await navigator.clipboard.write(data)
     }
   }
 }
