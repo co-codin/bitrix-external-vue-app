50dd46e561eb2a77f3fc839a8212852dbc70c7b2
Lenar Khusainov <work@lenarx.ru>
2022-02-25 10:03:59 +0300

    Load task

--- src/pages/tasks/ChangeTaskStatus.vue
+++ src/pages/tasks/ChangeTaskStatus.vue
@@ -2,6 +2,7 @@
   <div>
     <page-header h1="Изменение статуса задачи" :show-home-link="false"/>
     <page-loader v-if="loading"/>
+    <v-alert v-else-if="error" type="error" text v-text="error" />
     <template v-else>
       <v-form @submit.prevent="changeTaskStatus">
         <v-card>
@@ -34,6 +35,7 @@ export default {
   },
   data: () => ({
     loading: true,
+    error: null,
     taskStatusLabels: [
       { text: 'В оплате', value: 'В оплате' },
       { text: 'Ждем поставку', value: 'Ждем поставку' },
@@ -54,11 +56,14 @@ export default {
       const { options } = BX24.placement.info()
       const taskId = options?.ID ?? options?.TASK_ID // bitrix из списка задач отдает ID, а через карточку задачи TASK_ID
 
-      BX24.callMethod('tasks.task.get', { taskId }, (response) => {
-        console.log(response.data)
-        console.log(response.data())
-        console.log(response)
-        console.log(response.answer)
+      BX24.callMethod('tasks.task.get', { taskId, select: ['uf_task_status'] }, (response) => {
+        this.loading = false
+        if (response.data()) {
+          this.task = response.data().task
+
+          return
+        }
+        this.error = 'Произошла ошибка при получении данных'
       })
     },
     changeTaskStatus() {
