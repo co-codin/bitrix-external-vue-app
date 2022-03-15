3ac68f098e9e781f30b073d7ddf4da1b478548e3
ElijahCoding <cuiyeqing960904@gmail.com>
2022-03-15 13:28:07 +0300

    fixed

--- src/plugins/bitrix-wrapper.js
+++ src/plugins/bitrix-wrapper.js
@@ -1,6 +1,4 @@
 import Vue from 'vue'
 import BX24Wrapper from '@/utils/bx24-wrapper'
 
-Vue.prototype.$bxwrapper = function () {
-  return (new BX24Wrapper())
-}
+Vue.prototype.$bxwrapper = new BX24Wrapper()
