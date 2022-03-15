e72efde5fd140a96bdd0d7eaebb71ee3352d184b
Lenar Khusainov <work@lenarx.ru>
2022-03-15 14:13:53 +0300

    Fix filling statistics

--- src/pages/deals/FillingStatistics.vue
+++ src/pages/deals/FillingStatistics.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
-    <page-header h1="Статистика заполнения сделок" :show-home-link="false"/>
-    <v-btn @click="selectUser">
-      <span v-if="isUserSelected">
-        {{ manager.name }}
-      </span>
-      <span v-else>
-        Выбрать пользователя
-      </span>
-    </v-btn>
+    <v-card>
+      <v-card-title class="cursor-pointer" @click="selectUser">
+        <document-search-icon width="30" height="30" class="mr-1" />
+        {{ isUserSelected ? manager.name : 'Выберите ответственного менеджера' }}
+      </v-card-title>
+    </v-card>
 
     <v-card v-if="isUserSelected" class="mt-3">
+      <v-card-title>
+        Сделки
+      </v-card-title>
       <v-data-table
-        v-if="deals.length"
         item-key="id"
         :headers="headers"
         :items="deals"
@@ -25,22 +24,17 @@
             <a href="#" @click.prevent="openDeal(item.id)">{{ item.name }}</a>
           </div>
         </template>
-
       </v-data-table>
-      <v-alert v-else color="primary" text class="text-center">
-        Выберите ответственного менеджера
-      </v-alert>
     </v-card>
   </div>
 </template>
 
 <script>
-import PageHeader from '@/components/PageHeader'
+import DocumentSearchIcon from '@/components/heroicons/DocumentSearchIcon'
 
 export default {
-  name: 'FillingStatistics',
   components: {
-    PageHeader
+    DocumentSearchIcon
   },
   data: () => ({
     loading: false,
@@ -48,16 +42,18 @@ export default {
     deals: [
       { id: 10202, name: '#22102 / Максим / Москва', is_phone_filled: 'Да' }
     ],
+
     headers: [
-      { text: 'Сделка', align: 'left', value: 'name', sortable: false },
-      { text: 'Телефон заполнен?', align: 'left', value: 'is_phone_filled', sortable: false },
-      { text: '', sortable: false, align: 'right', value: 'action' }
-    ],
-    managers: [
-      { text: 'Иван Петров', value: 1 },
-      { text: 'Иван Петров', value: 2 },
-      { text: 'Иван Петров', value: 3 },
-      { text: 'Иван Петров', value: 4 }
+      { text: '', align: 'left', value: 'name', sortable: false },
+      { text: 'Указана компания', value: 'is', sortable: false },
+      { text: 'Указан ИНН', value: 'is', sortable: false },
+      { text: 'Указано имя', value: 'is', sortable: false },
+      { text: 'Указана почта', value: 'is', sortable: false },
+      { text: 'БД', value: 'is', sortable: false },
+      { text: 'Просрочен звонок', value: 'is', sortable: false },
+      { text: 'За последние 60 дней не было звонка', value: 'is', sortable: false },
+      { text: 'Звонок позже 60 дней', value: 'is', sortable: false },
+      { text: 'Указана сумма', value: 'is', sortable: false }
     ]
   }),
   computed: {
@@ -67,12 +63,19 @@ export default {
   },
   methods: {
     openDeal(dealId) {
+      BX24.openPath(`/crm/deal/details/${dealId}`)
       // open deal slider
     },
     selectUser() {
       BX24.selectUser((data) => {
         this.manager = { ...data }
+        this.loadDeals()
       })
+    },
+    loadDeals() {
+      this.loading = true
+      // loading deals for selected user
+      this.loading = false
     }
   }
 }
