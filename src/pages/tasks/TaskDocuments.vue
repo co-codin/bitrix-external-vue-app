3f13e84a851a6b258a23655787053d6d1669bb20
ElijahCoding <cuiyeqing960904@gmail.com>
2022-03-17 12:09:13 +0300

    testing

--- src/pages/tasks/TaskDocuments.vue
+++ src/pages/tasks/TaskDocuments.vue
@@ -1,101 +1,6 @@
 <template>
   <div>
-    <v-dialog v-model="dialog" scrollable max-width="1000px">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          small
-          v-bind="attrs"
-          class="mb-2"
-          v-on="on"
-        >
-          Добавить документы
-        </v-btn>
-      </template>
-
-      <v-card>
-        <v-card-title>
-          <span class="headline">
-            Массовая загрузка файлов к задаче
-          </span>
-
-        </v-card-title>
-        <v-card-text>
-          <div class="mb-2">
-            <v-file-input
-              id="file"
-              v-model="uploadedFile"
-              @change="handleFileUpload"
-            />
-          </div>
-
-          <div class="mb-7">
-            <v-form
-              @submit.prevent="uploadFiles"
-            >
-              <v-expansion-panels v-if="form.files.length" class="mb-2">
-                <v-expansion-panel v-for="(file, index) in form.files" :key="index" :class="{'error-block': formErrors.hasOwnProperty(`files.${index}.name`) || formErrors.hasOwnProperty(`files.${index}.type`)}">
-                  <v-expansion-panel-header class="title">
-                    {{ file.name }}
-                  </v-expansion-panel-header>
-                  <v-expansion-panel-content eager>
-                    <v-row>
-                      <v-col cols="12" sm="5" md="4" lg="2">
-                        <file-preview-icon :extension="file.extension">
-                          {{ file.name }}.{{ file.extension }}
-                        </file-preview-icon>
-                      </v-col>
-                      <v-col cols="12" sm="7" md="8" lg="10">
-                        <div>
-                          <v-text-field
-                            v-model="file.name"
-                            label="Название"
-                            dense
-                            :error="formErrors.hasOwnProperty(`files.${index}.name`)"
-                            :error-messages="formErrors[`files.${index}.name`]"
-                          />
-                          <v-select
-                            v-model="file.type"
-                            label="Выберите тип документа"
-                            :items="documentTypeLabels"
-                            dense
-                            :error="formErrors.hasOwnProperty(`files.${index}.type`)"
-                            :error-messages="formErrors[`files.${index}.type`]"
-                          />
-                          <v-text-field
-                            v-model="file.comment"
-                            label="Заметка"
-                            dens
-                            :error="formErrors.hasOwnProperty(`files.${index}.comment`)"
-                            :error-messages="formErrors[`files.${index}.comment`]"
-                          />
-                        </div>
-                      </v-col>
-                    </v-row>
-                    <v-divider class="my-2"/>
-                    <div class="text-center">
-                      <v-btn small color="red" dark @click="removeFile(index)">Удалить</v-btn>
-                    </div>
-                  </v-expansion-panel-content>
-                </v-expansion-panel>
-                <div class="text-right mt-2">
-                  <v-btn type="submit" color="primary" :loading="loadingFiles" :disabled="loadingFiles">
-                    Загрузить выбранные файлы ({{ form.files.length }})
-                  </v-btn>
-                </div>
-
-              </v-expansion-panels>
-            </v-form>
-          </div>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="red darken-1" text @click="close">Отмена</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <CreateTaskDocument @uploaded="getTaskFiles" />
     <v-card>
       <v-data-table
         v-if="files.length"
@@ -150,16 +55,15 @@
 import TrashIcon from '@/components/heroicons/TrashIcon'
 import EyeIcon from '@/components/heroicons/EyeIcon'
 import DownloadIcon from '@/components/heroicons/DownloadIcon'
-import FilePreviewIcon from '../../components/FilePreviewIcon'
-import * as Validator from 'validatorjs'
 import BX24Wrapper from '@/utils/bx24-wrapper'
+import CreateTaskDocument from '@/pages/tasks/components/CreateTaskDocument'
 
 export default {
   components: {
     TrashIcon,
     EyeIcon,
     DownloadIcon,
-    FilePreviewIcon
+    CreateTaskDocument
   },
   data: () => ({
     files: [],
@@ -171,21 +75,13 @@ export default {
       { text: '', sortable: false, align: 'right', value: 'action' }
     ],
     isLoading: false,
-    dialog: false,
     form: {
       files: []
     },
-    documentTypeLabels: [
-      { text: 'Счет', value: 1 },
-      { text: 'Договор', value: 2 },
-      { text: 'УПД', value: 3 }
-    ],
-    loadingFiles: false,
     rules: {
       'files.*.name': 'required|distinct',
       'files.*.type': 'required'
-    },
-    formErrors: {}
+    }
   }),
   async mounted() {
     await this.getTaskFiles()
@@ -204,21 +100,8 @@ export default {
 
         this.files = task.UF_TASK_WEBDAV_FILES
       } catch (e) {
-        console.log(e)
+        this.$snackbar('Произошла ошибка при загрузке файлов')
       }
-    },
-    removeFile(index) {
-      this.form.files.splice(index, 1)
-    },
-    handleFileUpload(file) {
-      this.form.files.push({
-        file: document.getElementById('file'),
-        name: file.name.replace(/\.[^/.]+$/, ''),
-        type: null,
-        comment: '',
-        extension: file.name.split('.').pop()
-      })
-      this.uploadedFile = []
     },
     async downloadFile(item) {
       try {
@@ -233,7 +116,7 @@ export default {
         link.click()
         link.remove()
       } catch (e) {
-        console.log(e)
+        this.$snackbar('Произошла ошибка при качании файла')
       }
 
     },
@@ -245,7 +128,7 @@ export default {
 
         window.open(file.DETAIL_URL, '_blank')
       } catch (e) {
-        console.log(e)
+        this.$snackbar('Произошла ошибка при просмотра файла')
       }
     },
     async deleteFile(item) {
@@ -267,82 +150,11 @@ export default {
           this.$snackbar('Файл успешно удален')
           await this.getTaskFiles()
         } catch (e) {
-          console.log(e)
+          this.$snackbar('Произошла ошибка при отвязки файла')
         }
       } catch (e) {
-        console.log(e)
+        this.$snackbar('Произошла ошибка при удалении файла')
       }
-    },
-    close() {
-      this.dialog = false
-      this.form.files = []
-    },
-    async uploadFiles() {
-      Validator.register('distinct', (value, requirement, attribute) => {
-        return this.form.files.filter((file) => {
-          return file.name === value
-        }).length <= 1
-      })
-      const validation = await new Validator(this.form, this.rules, {
-        'required': ':attribute обязательно для заполнения.',
-        'distinct': 'Название должно быть уникальным.'
-      })
-
-      for (let i = 0; i < this.form.files.length; i++) {
-        validation.setAttributeNames({
-          [`files.${i}.name`]: 'Название',
-          [`files.${i}.type`]: 'Тип'
-        })
-      }
-
-      if (validation.fails()) {
-        this.formErrors = validation.errors.errors
-
-        return
-      }
-
-      this.loadingFiles = true
-
-      const batch = this.form.files.map((file) => {
-        return [
-          'disk.storage.uploadfile',
-          {
-            id: process.env.VUE_APP_STORAGE_ID,
-            fileContent: file.file,
-            data: {
-              NAME: file.name + '.' + file.extension,
-              TYPE: file.type,
-              COMMENT: file.comment
-            }
-          }
-        ]
-      })
-
-      try {
-        let batchResponse = await (new BX24Wrapper()).callBatch(batch, false)
-
-        batchResponse = batchResponse.map((batch) => {
-          return [
-            'tasks.task.files.attach',
-            {
-              taskId: this.taskId,
-              fileId: batch.ID
-            }
-          ]
-        })
-        try {
-          await (new BX24Wrapper()).callBatch(batchResponse, false)
-        } catch (e) {
-          console.log(e)
-          this.$snackbar('Произошла ошибка')
-        }
-      } catch (e) {
-        this.loadingFiles = false
-        this.$snackbar('Произошла ошибка')
-      }
-
-      this.loadingFiles = false
-      this.dialog = false
     }
   }
 }
---
+++ src/pages/tasks/components/CreateTaskDocument.vue
@@ -0,0 +1,212 @@
+<template>
+  <v-dialog v-model="dialog" scrollable max-width="1000px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        color="primary"
+        dark
+        small
+        v-bind="attrs"
+
+        class="mb-2"
+        v-on="on"
+      >
+        Добавить документы
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title>
+        <span class="headline">
+          Массовая загрузка файлов к задаче
+        </span>
+
+      </v-card-title>
+      <v-card-text>
+        <div class="mb-2">
+          <v-file-input
+            id="file"
+            v-model="uploadedFile"
+            @change="handleFileUpload"
+          />
+        </div>
+
+        <div class="mb-7">
+          <v-form
+            @submit.prevent="handleSubmit"
+          >
+            <v-expansion-panels v-if="form.files.length" class="mb-2">
+              <v-expansion-panel v-for="(file, index) in form.files" :key="index" :class="{'error-block': formErrors.hasOwnProperty(`files.${index}.name`) || formErrors.hasOwnProperty(`files.${index}.type`)}">
+                <v-expansion-panel-header class="title">
+                  {{ file.name }}
+                </v-expansion-panel-header>
+                <v-expansion-panel-content eager>
+                  <v-row>
+                    <v-col cols="12" sm="5" md="4" lg="2">
+                      <file-preview-icon :extension="file.extension">
+                        {{ file.name }}.{{ file.extension }}
+                      </file-preview-icon>
+                    </v-col>
+                    <v-col cols="12" sm="7" md="8" lg="10">
+                      <div>
+                        <v-text-field
+                          v-model="file.name"
+                          label="Название"
+                          dense
+                          :error="formErrors.hasOwnProperty(`files.${index}.name`)"
+                          :error-messages="formErrors[`files.${index}.name`]"
+                        />
+                        <v-select
+                          v-model="file.type"
+                          label="Выберите тип документа"
+                          :items="documentTypeLabels"
+                          dense
+                          :error="formErrors.hasOwnProperty(`files.${index}.type`)"
+                          :error-messages="formErrors[`files.${index}.type`]"
+                        />
+                        <v-text-field
+                          v-model="file.comment"
+                          label="Заметка"
+                          dens
+                          :error="formErrors.hasOwnProperty(`files.${index}.comment`)"
+                          :error-messages="formErrors[`files.${index}.comment`]"
+                        />
+                      </div>
+                    </v-col>
+                  </v-row>
+                  <v-divider class="my-2"/>
+                  <div class="text-center">
+                    <v-btn small color="red" dark @click="removeFile(index)">Удалить</v-btn>
+                  </div>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+              <div class="text-right mt-2">
+                <v-btn type="submit" color="primary" :loading="loadingFiles" :disabled="loadingFiles">
+                  Загрузить выбранные файлы ({{ form.files.length }})
+                </v-btn>
+              </div>
+
+            </v-expansion-panels>
+          </v-form>
+        </div>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="red darken-1" text @click="close">Отмена</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import FilePreviewIcon from '@/components/FilePreviewIcon'
+import * as Validator from 'validatorjs'
+import BX24Wrapper from '@/utils/bx24-wrapper'
+
+export default {
+  components: {
+    FilePreviewIcon
+  },
+  data: () => ({
+    dialog: false,
+    uploadedFile: null,
+    loadingFiles: false,
+    form: {
+      files: []
+    },
+    formErrors: {},
+    documentTypeLabels: [
+      { text: 'Счет', value: 1 },
+      { text: 'Договор', value: 2 },
+      { text: 'УПД', value: 3 }
+    ]
+  }),
+  methods: {
+    handleFileUpload(file) {
+      this.form.files.push({
+        file: document.getElementById('file'),
+        name: file.name.replace(/\.[^/.]+$/, ''),
+        type: null,
+        comment: '',
+        extension: file.name.split('.').pop()
+      })
+      this.uploadedFile = []
+    },
+    removeFile(index) {
+      this.form.files.splice(index, 1)
+    },
+    async handleSubmit() {
+      this.loadingFiles = true
+      await this.uploadFiles()
+      this.loadingFiles = false
+    },
+    close() {
+      this.dialog = false
+      this.form.files = []
+    },
+    async uploadFiles() {
+      Validator.register('distinct', (value, requirement, attribute) => {
+        return this.form.files.filter((file) => {
+          return file.name === value
+        }).length <= 1
+      })
+      const validation = await new Validator(this.form, this.rules, {
+        'required': ':attribute обязательно для заполнения.',
+        'distinct': 'Название должно быть уникальным.'
+      })
+
+      for (let i = 0; i < this.form.files.length; i++) {
+        validation.setAttributeNames({
+          [`files.${i}.name`]: 'Название',
+          [`files.${i}.type`]: 'Тип'
+        })
+      }
+
+      if (validation.fails()) {
+        this.formErrors = validation.errors.errors
+
+        return
+      }
+
+      const batch = this.form.files.map((file) => {
+        return [
+          'disk.storage.uploadfile',
+          {
+            id: process.env.VUE_APP_STORAGE_ID,
+            fileContent: file.file,
+            data: {
+              NAME: file.name + '.' + file.extension,
+              TYPE: file.type,
+              COMMENT: file.comment
+            }
+          }
+        ]
+      })
+
+      try {
+        let batchResponse = await (new BX24Wrapper()).callBatch(batch, false)
+
+        batchResponse = batchResponse.map((batch) => {
+          return [
+            'tasks.task.files.attach',
+            {
+              taskId: this.taskId,
+              fileId: batch.ID
+            }
+          ]
+        })
+        try {
+          await (new BX24Wrapper()).callBatch(batchResponse, false)
+          this.$emit('uploaded')
+        } catch (e) {
+          this.$snackbar('Произошла ошибка')
+        }
+      } catch (e) {
+        this.$snackbar('Произошла ошибка')
+      }
+
+      this.dialog = false
+    }
+  }
+}
+</script>
