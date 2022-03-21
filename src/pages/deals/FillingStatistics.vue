53b2e04fe9be8df1cff1ac7a996bfc934b59e008
ElijahCoding <cuiyeqing960904@gmail.com>
2022-03-21 15:14:01 +0300

    testing

--- src/pages/deals/FillingStatistics.vue
+++ src/pages/deals/FillingStatistics.vue
@@ -135,11 +135,21 @@ export default {
         ]
       }))
 
-      const contacts = await (new BX24Wrapper()).callLongBatch('crm.contact.list', {
-        filter: { 'ID': dealContacts.map((item) => item.CONTACT_ID) }
-      })
+      const contacts = await (new BX24Wrapper()).callLongBatch(dealContacts.map((dealContact) => {
+        return [
+          'crm.contact.list', { filter: { 'ID': dealContact.CONTACT_ID } }
+        ]
+      }))
+
+      const singleContacts = await (new BX24Wrapper()).callLongBatch(dealContacts.map((dealContact) => {
+        return [
+          'crm.contact.get', { id: dealContact.CONTACT_ID }
+        ]
+      }))
 
       console.log(contacts)
+
+      console.log(singleContacts)
 
       // deals.forEach((deal) => {
       //   (new BX24Wrapper()).callMethod('crm.deal.contact.items.get', {
