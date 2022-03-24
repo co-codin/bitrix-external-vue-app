93f3d06127a8b67474aa023cf5b913a4e1eaeade
ElijahCoding <cuiyeqing960904@gmail.com>
2022-03-24 15:07:06 +0300

    fixed

--- src/pages/deals/FillingStatistics.vue
+++ src/pages/deals/FillingStatistics.vue
@@ -185,7 +185,7 @@ export default {
         }).includes(true)
 
         const hasNoOverdueCalls = activities[index].map((activity) => {
-          return ((new Date()).getTime() - (new Date(activity.DEADLINE)).getTime()) / (1000 * 3600 * 24) > 1
+          return ((new Date()).getTime() - (new Date(activity.END_TIME)).getTime()) / (1000 * 3600 * 24) > 1
         }).includes(true)
 
         const hasPlannedCalls = activities[index].map((activity) => {
