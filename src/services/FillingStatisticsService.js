9f39c426d761d33d456bec6232bbd20968c0f85e
Lenar Khusainov <l.khusainov@medeq.ru>
2022-04-10 15:04:10 +0300

    no name

--- src/services/FillingStatisticsService.js
+++ src/services/FillingStatisticsService.js
@@ -64,7 +64,7 @@ export default class FillingStatisticsService {
         name: deal.TITLE,
         has_company_name: ['NEW', 'DETAILS'].includes(deal.STAGE_ID) || !!(companiesById?.[deal.COMPANY_ID]?.TITLE?.length),
         has_inn: ['NEW', 'DETAILS'].includes(deal.STAGE_ID) || !! companyRequisites?.[deal.COMPANY_ID],
-        has_name:  !! currentDealContacts.filter((contact) => !! contact.NAME?.length || !! contact.LAST_NAME?.length || contact.SECOND_NAME?.length).length,
+        has_name: !! currentDealContacts.filter((contact) => contact.NAME !== 'Без имени' && (!! contact.NAME?.length || !! contact.LAST_NAME?.length || contact.SECOND_NAME?.length)).length,
         has_email: ['NEW', 'DETAILS'].includes(deal.STAGE_ID) || currentDealContacts?.map((contact) => contact?.HAS_EMAIL).includes('Y') || companiesById?.[deal.COMPANY_ID]?.HAS_EMAIL === 'Y',
         has_sum: ['NEW', 'DETAILS'].includes(deal.STAGE_ID) ? !! deal.UF_PROCEEDS : !! deal.UF_PROCEEDS && deal.UF_PROCEEDS !== '100|RUB',
         has_client_type: !! deal.UF_CRM_CLIENT_TYPE,
