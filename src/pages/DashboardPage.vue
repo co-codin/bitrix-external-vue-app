964056cc0729a2d62637e6a2f5f4290d9e142aa3
Lenar Khusainov <l.khusainov@medeq.ru>
2022-06-09 16:41:48 +0300

    Fix

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,25 +1,23 @@
 <template>
-  <v-container>
-    <div class="d-flex flex-grow-1 flex-column">
-      <draggable class="row flex-grow-0 row--dense">
-        <v-col cols="12" md="6" xl="6">
-          <academy class="h-full" />
-        </v-col>
-        <v-col cols="12" md="6" xl="3">
-          <document-list-card class="h-full" />
-        </v-col>
-        <v-col cols="12" md="6" xl="3">
-          <booking-list-card class="h-full" />
-        </v-col>
-        <v-col cols="12" md="6" xl="3">
-          <incoming-payment-list-card class="h-full" />
-        </v-col>
-        <v-col cols="12" md="6" xl="3">
-          <outgoing-payment-list-card class="h-full" />
-        </v-col>
-      </draggable>
-    </div>
-  </v-container>
+  <div class="d-flex flex-grow-1 flex-column">
+    <draggable class="row flex-grow-0 row--dense">
+      <v-col cols="12" md="6" xl="6">
+        <academy class="h-full" />
+      </v-col>
+      <v-col cols="12" md="6" xl="3">
+        <document-list-card class="h-full" />
+      </v-col>
+      <v-col cols="12" md="6" xl="3">
+        <booking-list-card class="h-full" />
+      </v-col>
+      <v-col cols="12" md="6" xl="3">
+        <incoming-payment-list-card class="h-full" />
+      </v-col>
+      <v-col cols="12" md="6" xl="3">
+        <outgoing-payment-list-card class="h-full" />
+      </v-col>
+    </draggable>
+  </div>
 </template>
 
 <script>
