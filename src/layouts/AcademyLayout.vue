1a6010ae6adcd0c4ddda66a702fa297c4f6c62fc
Lenar Khusainov <l.khusainov@medeq.ru>
2022-08-26 13:24:24 +0300

    search

--- src/layouts/AcademyLayout.vue
+++ src/layouts/AcademyLayout.vue
@@ -39,7 +39,36 @@
     </v-btn>
     <v-dialog v-model="showCommandPalette" max-width="1000px" content-class="search-dialog">
       <v-card>
-        <v-autocomplete solo placeholder="Поиск по материалам" hide-details prepend-inner-icon="search"/>
+        <v-autocomplete
+          autofocus
+          :loading="isLoading"
+          solo
+          item-text="name"
+          item-value="id"
+          placeholder="Поиск по материалам"
+          hide-details
+          prepend-inner-icon="search"
+          :items="items"
+          :search-input.sync="search"
+          cache-items
+          v-bind="$props"
+          @change="search = ''"
+        >
+          <template #item="{ item }">
+            <v-list-item link :to="{ name: 'wiki.materials.show', params: { id: item.material_id || item.id }, hash: item.material && `#block-${item.id}` }" @click="hideCommandPalette">
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ item.material ? item.material.name : item.name }}
+                </v-list-item-title>
+                <template v-if="item.material && item.type === 'block'">
+                  <v-list-item-subtitle>
+                    {{ item.name }}
+                  </v-list-item-subtitle>
+                </template>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-autocomplete>
       </v-card>
     </v-dialog>
   </v-container>
@@ -47,6 +76,7 @@
 
 <script>
 import MaterialTree from '@/components/wiki/MaterialTree'
+import debounce from 'lodash/debounce'
 
 export default {
   components: {
@@ -57,8 +87,17 @@ export default {
   },
   data: () => ({
     fab: false,
-    showCommandPalette: false
+    showCommandPalette: false,
+    isLoading: false,
+    search: null,
+    items: []
+
   }),
+  watch: {
+    search(value) {
+      this.searchItems(value)
+    }
+  },
   methods: {
     onScroll(e) {
       if (typeof window === 'undefined') return
@@ -68,7 +107,23 @@ export default {
     },
     openCommandPalette() {
       this.showCommandPalette = true
-    }
+    },
+    hideCommandPalette() {
+      this.showCommandPalette = false
+    },
+    searchItems: debounce(async function (query) {
+      this.isLoading = true
+
+      const { data } = await this.$r2d2.get('/wiki/search', {
+        params: {
+          term: query
+        }
+      })
+
+      this.items = data.results
+
+      this.isLoading = false
+    }, 300)
   }
 }
 </script>
