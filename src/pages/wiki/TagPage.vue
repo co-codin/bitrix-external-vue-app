26e1ed44004b3ce4f1c0fb69e06a25219c327bff
Lenar Khusainov <work@lenarx.ru>
2022-09-02 09:04:34 +0300

    Update tag page on change tag

--- src/pages/wiki/TagPage.vue
+++ src/pages/wiki/TagPage.vue
@@ -25,26 +25,37 @@
 <script>
 import Tag from '../../models/Tag'
 import MaterialBlock from '../../components/wiki/MaterialBlock'
+import HasLoadingBar from '@/mixins/hasLoadingBar'
+import HasTitle from '@/mixins/hasTitle'
 
 export default {
   components: {
     MaterialBlock
   },
+  mixins: [
+    HasLoadingBar, HasTitle
+  ],
   title() {
     return `${this.tag?.name || 'Страница тега'}`
   },
   data: () => ({
-    tag: null,
-    loading: true
+    tag: null
   }),
   async created() {
-    console.log(this.$route)
-    try {
-      this.tag = await Tag.with('materialBlocks.material').$find(this.$route.params.id)
-      this.loading = false
+    await this.loadTag(this.$route.params.id)
+  },
+  beforeRouteUpdate(to, from, next) {
+    if (to.name === 'wiki.tags.show') {
+      this.loadTag(to.params.id)
     }
-    catch (e) {
-      this.loading = false
+
+    next()
+  },
+  methods: {
+    async loadTag(id) {
+      this.showLoading()
+      this.tag = await Tag.with('materialBlocks.material').$find(id)
+      this.hideLoading()
     }
   }
 }
